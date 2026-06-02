287d61df13ec8c009eccc88f9ec4055bc2a2fac0
septiandwica <septiandwica03@gmail.com>
2026-06-02 20:33:28 +0700

    chore: bump version to 2026060209 for settings cleanup

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_president';
 
 // This is the version of the plugin.
-$plugin->version = 2026060208;
+$plugin->version = 2026060209;
 
 // This is the named version.
 $plugin->release = '5.0.0';
