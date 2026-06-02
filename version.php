b0c29535851e864acddc10a32cc9a84d363faf7e
septiandwica <septiandwica03@gmail.com>
2026-06-02 16:22:46 +0700

    refactor: remove page title from custom page template and bump plugin version

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_president';
 
 // This is the version of the plugin.
-$plugin->version = 2026060201;
+$plugin->version = 2026060202;
 
 // This is the named version.
 $plugin->release = '5.0.0';
